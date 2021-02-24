6b212250668a777af2e7882a03c5491728b1336b
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-02-24 17:47:35 +0100

    Esercizio completo
    sort (indifferente se inizia per minuscola o maiuscola), inserita possibilità di aggiungere altri cognomi da input con aggiornamento della lista e della posizione, aggiunto tasto reset.

---
+++ js/main.js
@@ -0,0 +1,79 @@
+//array con cognomi
+var surnameList = [
+    "Bianchi",
+    "Rossi",
+    "Duzioni",
+    "Balsano",
+    "Verdi"
+];
+
+//variabile posizione utente
+var userPosition = 1;
+
+//variabile titolo lista
+var listTitle = document.getElementById('ms_list_title');
+
+// inizializzazione variabile form input
+var userInputForm = document.getElementById('surname');
+
+//inizializzazione variabile submit
+var submit = document.getElementById('submit');
+
+//inizializzazione variabile reset
+var reset = document.getElementById('reset');
+
+
+submit.addEventListener('click',
+    function () {
+
+        //in caso di succevvive aggiunte di cognomi cancella la precendente lista
+        document.getElementById('ms_surname_list').innerHTML = ' ';
+
+        // inizializzazione variabile cognome utente
+        var userSurname = document.getElementById('surname').value;
+        console.log(userSurname);
+
+        //aggiunta cognome utente all'array
+        surnameList.push(userSurname);
+
+        //copia array con cognomi da ordinare 
+        var surnameListSorted = surnameList;
+
+        //sort ordine alfabetico del secondo array
+        surnameListSorted.sort(function (a, b) {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            if (a == b) return 0;
+            if (a > b) return 1;
+            return -1;
+        })
+
+        console.log(surnameListSorted);
+        listTitle.style.display = "block";
+
+        //display titolo della lista
+
+        // stampa lista cognomi ordinata e ricerca posizione utente
+        for(var i = 0; i < surnameListSorted.length; i++) {
+            document.getElementById('ms_surname_list').innerHTML += "<li>" + surnameListSorted[i] + "</li>";
+            if(surnameListSorted[i] === userSurname) {
+                document.getElementById('ms_user_surname_position').innerHTML = userSurname + " sei in posizione " + userPosition;
+            } else {
+                userPosition += 1;
+            }
+        }
+
+        //reset posizione utente per successivi inserimenti
+        userPosition = 1;
+
+        //ripulire l'input
+        userInputForm.value = '';
+    }
+);
+
+
+reset.addEventListener('click',
+    function () {
+        location.reload();
+    }
+);
